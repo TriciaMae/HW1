0b1301a96c479bc2278add04c85382054bd26c84
TriciaMae <58018345+TriciaMae@users.noreply.github.com>
2020-02-18 09:36:20 +0800

    Added func to XML btn

--- Form1.vb
+++ Form1.vb
@@ -15,7 +15,27 @@ Public Class Form1
         'Writing the entered info into the txt file
         Dim info As Byte() = New UTF8Encoding(True).GetBytes("Name: " + name + vbCrLf + "Age: " + age + vbCrLf + "Address: " + address)
         fs.Write(info, 0, info.Length)
-        MsgBox("Successfully created .txt file!", MsgBoxStyle.Information, "Txt File")
+        MsgBox("Successfully created Text file!", MsgBoxStyle.Information, "Txt File")
+        fs.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Creating .txt file with file path
+        Dim path As String = "c:\temp\XML Format.xml"
+        Dim fs As FileStream = File.Create(path)
+        Dim name, age, address As String
+
+        'Getting entered info
+        name = NameBox.Text
+        age = AgeBox.Text
+        address = AddressBox.Text
+
+        'Writing the entered info into the txt file
+        Dim info As Byte() = New UTF8Encoding(True).GetBytes("<article lang=''''>" + vbCrLf + "     <para>Name: " + name + "</para>" + vbCrLf +
+              "     <para>Age: " + age + "</para>" + vbCrLf + "     <para>Address: " + address + "</para>" +
+               vbCrLf + "</article>")
+        fs.Write(info, 0, info.Length)
+        MsgBox("Successfully created XML file!", MsgBoxStyle.Information, "XML File")
         fs.Close()
     End Sub
 End Class
